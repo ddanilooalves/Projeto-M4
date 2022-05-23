c6d0aff6ad8cb6828a729e173904980aaf0d315e
ddanilooalves <daniloalves0512@gmail.com>
2022-05-23 17:58:33 -0300

    alt

--- src/table/dto/create-table.dto.ts
+++ src/table/dto/create-table.dto.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsPositive, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTableDto {
